1556b49baae7e12a6a7bcb56b4ecbb7ce2ab9075
Wani <devwani93@gmail.com>
2022-06-07 10:59:39 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/test-git-actions/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/test-git-actions/' : '/',
   outputDir: 'dist'
 }
